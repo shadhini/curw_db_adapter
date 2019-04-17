c479951bffa48d3054935811f7bb326d78fba959
shadhini <jshadhiniaseka@gmail.com>
2019-04-17 21:17:40 +0530

    debug logger

--- db_adapter/logger/logger.py
+++ db_adapter/logger/logger.py
@@ -2,7 +2,7 @@ import logging
 import logging.config
 import yaml
 
-with open('config.yaml', 'r') as f:
+with open('../../db_adapter/logger/config.yaml', 'r') as f:
     config = yaml.safe_load(f.read())
     logging.config.dictConfig(config)
 
