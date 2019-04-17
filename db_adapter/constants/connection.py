58b2acc08031aa3095cb4ad04742deb3c8986437
shadhini <jshadhiniaseka@gmail.com>
2019-04-17 20:43:55 +0530

    update logger config file

--- db_adapter/constants/connection.py
+++ db_adapter/constants/connection.py
@@ -2,14 +2,14 @@ DIALECT_MYSQL = "mysql"
 DRIVER_PYMYSQL = "pymysql"
 
 # ---------- CUrW Fcst Database --------------
-# CURW_FCST_USERNAME = "root"
-# CURW_FCST_PASSWORD = "cfcwm07"
-# CURW_FCST_HOST = "35.230.102.148"
-# CURW_FCST_PORT = 3306
-# CURW_FCST_DATABASE = "test_local-schema"
-
 CURW_FCST_USERNAME = "root"
-CURW_FCST_PASSWORD = "password"
-CURW_FCST_HOST = "127.0.0.1"
+CURW_FCST_PASSWORD = "cfcwm07"
+CURW_FCST_HOST = "35.230.102.148"
 CURW_FCST_PORT = 3306
 CURW_FCST_DATABASE = "curw_fcst"
+
+# CURW_FCST_USERNAME = "root"
+# CURW_FCST_PASSWORD = "password"
+# CURW_FCST_HOST = "127.0.0.1"
+# CURW_FCST_PORT = 3306
+# CURW_FCST_DATABASE = "curw_fcst"
