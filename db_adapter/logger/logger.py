e60a0f6bd9bc7b16530e9266c084a7d39ce4f9d3
shadhini <jshadhiniaseka@gmail.com>
2019-04-18 08:11:10 +0530

    debug logger

--- db_adapter/logger/logger.py
+++ db_adapter/logger/logger.py
@@ -1,8 +1,11 @@
 import logging
 import logging.config
 import yaml
+import json
 
-with open('logger_config.yaml', 'r') as f:
+db_config = json.loads(open('db_config.json').read())
+
+with open('{}/db_adapter/logger/config.yaml'.format(db_config["HOME"]), 'r') as f:
     config = yaml.safe_load(f.read())
     logging.config.dictConfig(config)
 
