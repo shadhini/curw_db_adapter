aa34e0fcfc6b7f8829c12bfc75b35cf24ae13397
shadhini <jshadhiniaseka@gmail.com>
2019-04-29 02:48:35 +0530

    fine tune before running tests

--- db_adapter/logger/logger.py
+++ db_adapter/logger/logger.py
@@ -3,7 +3,7 @@ import logging.config
 import yaml
 
 
-with open('logger_config.yaml', 'r') as f:
+with open('/home/shadhini/dev/repos/shadhini/wrfv3_data_pusher/logger_config.yaml', 'r') as f:
     config = yaml.safe_load(f.read())
     logging.config.dictConfig(config)
 
