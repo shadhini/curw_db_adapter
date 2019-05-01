dc8e1af8b5ab2c53c7b856d90ee9eee914df46e3
shadhini <jshadhiniaseka@gmail.com>
2019-05-02 04:40:58 +0530

    update base exports

--- db_adapter/base/__init__.py
+++ db_adapter/base/__init__.py
@@ -1,5 +1,5 @@
 from .sqlalchemy_base import CurwFcstBase, CurwObsBase
 from .sqlalchemy_base import get_engine, get_sessionmaker
 
-from .pymysql_base import Pool
+from .pymysql_base import get_Pool
 
