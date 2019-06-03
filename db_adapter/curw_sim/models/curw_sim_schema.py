bacd94e710c6951724e22dcce12a9ae28a2857e1
shadhini <jshadhiniaseka@gmail.com>
2019-06-03 12:02:44 +0530

    update curw sim schema

--- db_adapter/curw_sim/models/curw_sim_schema.py
+++ db_adapter/curw_sim/models/curw_sim_schema.py
@@ -10,7 +10,7 @@ from db_adapter.base import CurwSimBase
 class GridMap(CurwSimBase):
     __tablename__ = 'grid_map'
 
-    grid_id = Column(VARCHAR(45), nullable=False, primary_key=True, unique=True, autoincrement=False)
+    grid_id = Column(VARCHAR(45), nullable=False, primary_key=True, autoincrement=False)
     obs1 = Column(INTEGER(11), nullable=False)
     obs2 = Column(INTEGER(11), nullable=False)
     obs3 = Column(INTEGER(11), nullable=False)
@@ -33,7 +33,7 @@ class Run(CurwSimBase):
     longitude = Column(DOUBLE, nullable=False)
     model = Column(VARCHAR(25), nullable=False)
     method = Column(VARCHAR(100), nullable=False)
-    grid_id = Column(INTEGER(11), ForeignKey(GridMap.grid_id), nullable=False)
+    grid_id = Column(VARCHAR(45), ForeignKey(GridMap.grid_id), nullable=False)
 
     data_relationship = relationship("Data", back_populates="id_relationship", cascade="all, delete, delete-orphan")
 
