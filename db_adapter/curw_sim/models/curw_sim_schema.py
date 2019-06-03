45f43fa83f27b98a59c35b554509a7170a8bf40c
shadhini <jshadhiniaseka@gmail.com>
2019-06-03 10:42:41 +0530

    update curw sim schema

--- db_adapter/curw_sim/models/curw_sim_schema.py
+++ db_adapter/curw_sim/models/curw_sim_schema.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import relationship
 from db_adapter.base import CurwSimBase
 
 
-class Run_Mean(CurwSimBase):
-    __tablename__ = 'run_mean'
+class Run(CurwSimBase):
+    __tablename__ = 'run'
 
     id = Column(VARCHAR(64), nullable=False, primary_key=True, unique=True)
     latitude = Column(DOUBLE, nullable=False)
@@ -18,7 +18,7 @@ class Run_Mean(CurwSimBase):
 
     data_relationship = relationship("Data", back_populates="id_relationship", cascade="all, delete, delete-orphan")
 
-    grid_relationship = relationship('Grid_Map', foreign_keys='Run_Mean.grid_id', back_populates="run_relationship")
+    grid_relationship = relationship('Grid_Map', foreign_keys='Run.grid_id', back_populates="run_relationship")
 
     def __repr__(self):
         return "<Run_Mean(id='%s', latitude='%s', longitude='%s', model='%s', method='%s')>" \
@@ -66,11 +66,11 @@ class Run_Min(CurwSimBase):
 class Data(CurwSimBase):
     __tablename__='data'
 
-    id = Column(VARCHAR(64), ForeignKey(Run_Mean.id, Run_Max.id, Run_Min.id), nullable=False, primary_key=True)
+    id = Column(VARCHAR(64), ForeignKey(Run.id, Run_Max.id, Run_Min.id), nullable=False, primary_key=True)
     time = Column(DATETIME, nullable=False, primary_key=True)
     value = Column(DECIMAL(8, 3), nullable=False)
 
-    id_relationship = relationship('Run_Mean', foreign_keys='Data.id', back_populates="data_relationship")
+    id_relationship = relationship('Run', foreign_keys='Data.id', back_populates="data_relationship")
     max_id_relationship = relationship('Run_Max', foreign_keys='Data.id', back_populates="data_relationship")
     min_id_relationship = relationship('Run_Min', foreign_keys='Data.id', back_populates="data_relationship")
 
@@ -89,7 +89,7 @@ class Grid_Map(CurwSimBase):
     obs3 = Column(INTEGER(11), nullable=False)
     fcst = Column(INTEGER(11), nullable=False)
 
-    run_relationship = relationship("Run_Mean", back_populates="grid_relationship", cascade="all, delete, delete-orphan")
+    run_relationship = relationship("Run", back_populates="grid_relationship", cascade="all, delete, delete-orphan")
     run_max_relationship = relationship("Run_Max", back_populates="max_grid_relationship", cascade="all, delete, delete-orphan")
     run_min_relationship = relationship("Run_Min", back_populates="min_grid_relationship", cascade="all, delete, delete-orphan")
 
