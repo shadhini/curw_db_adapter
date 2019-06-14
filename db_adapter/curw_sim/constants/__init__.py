7f3534a3ddf78516c1d8728c0300db93b6ca1dc1
shadhini <jshadhiniaseka@gmail.com>
2019-06-14 13:12:45 +0530

    uodate function to retrieve hash id for a run entry

--- db_adapter/curw_sim/constants/__init__.py
+++ db_adapter/curw_sim/constants/__init__.py
@@ -1,1 +1,1 @@
-from .model_names import FLO2D_250, FLO2D_150, FLO2D_30
+from .model_names import FLO2D_250, FLO2D_150, FLO2D_30, HecHMS
--- db_adapter/curw_sim/constants/model_names.py
+++ db_adapter/curw_sim/constants/model_names.py
@@ -1,3 +1,4 @@
 FLO2D_250 = "flo2d_250"
 FLO2D_150 = "flo2d_150"
 FLO2D_30 = "flo2d_30"
+HecHMS = "hechms"
