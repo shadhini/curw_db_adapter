89a5fcc8504a0b08d923d7428a5f3af32c7d6a50
shadhini <jshadhiniaseka@gmail.com>
2019-06-15 05:44:58 +0530

    convert fcst rainfall series into 5 mins timeseries

--- db_adapter/curw_sim/common/common_utils.py
+++ db_adapter/curw_sim/common/common_utils.py
@@ -63,7 +63,10 @@ def convert_15_min_ts_to_5_mins_ts(newly_extracted_timeseries, expected_start=No
             current_timestamp = current_timestamp + timedelta(minutes=5)
         else:
             print("Case 3", current_timestamp)
-            processed_ts.append([current_timestamp, -99999])
+            if timedelta(minutes=0) < (newly_extracted_timeseries[extracted_ts_index][0] - current_timestamp) < timedelta(minutes=15):
+                processed_ts.append([current_timestamp, newly_extracted_timeseries[extracted_ts_index][1] / 3])
+            else:
+                processed_ts.append([current_timestamp, -99999])
             current_timestamp = current_timestamp + timedelta(minutes=5)
 
     return processed_ts
