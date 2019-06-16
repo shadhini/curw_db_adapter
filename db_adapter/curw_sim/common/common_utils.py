9df90e3628ac7e1bb6d5580816ec10e04feb2bc5
shadhini <jshadhiniaseka@gmail.com>
2019-06-16 16:13:21 +0530

    update scripts used to update fcst data for curw_sim to include average of timeseries of any number of models given as user defined parameters

--- db_adapter/curw_sim/common/common_utils.py
+++ db_adapter/curw_sim/common/common_utils.py
@@ -98,7 +98,7 @@ def average_timeseries(timeseries):
         for i in range(len(timeseries)):
             count = len(timeseries[i])-1
             print((sum(timeseries[i][1:])/count))
-            avg_timeseries.append([timeseries[i][0], "%.3f" % (sum(timeseries[i][1:])/count)])
+            avg_timeseries.append([timeseries[i][0], '%.3f' % (sum(timeseries[i][1:])/count)])
 
     return avg_timeseries
 
