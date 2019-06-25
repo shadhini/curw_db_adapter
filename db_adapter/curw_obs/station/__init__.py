eeb350751a28c96c13066d917f0b3fb0faae950a
shadhini <jshadhiniaseka@gmail.com>
2019-06-25 11:23:37 +0530

    debug station_utils

--- db_adapter/curw_obs/station/__init__.py
+++ db_adapter/curw_obs/station/__init__.py
@@ -1,4 +1,3 @@
 from .station_utils import get_station_id, get_station_by_id, add_station, delete_station_by_id, delete_station,\
-    add_stations, \
-    add_wrfv3_stations, get_wrfv3_stations
+    add_stations
 from .station_enum import StationEnum
