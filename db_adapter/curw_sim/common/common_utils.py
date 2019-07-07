f6e80ddb8f858fce4d5ad69fcda3bbe32278e3ef
shadhini <jshadhiniaseka@gmail.com>
2019-07-07 16:26:25 +0530

    debug index out of range issue in append_ts function

--- db_adapter/curw_sim/common/common_utils.py
+++ db_adapter/curw_sim/common/common_utils.py
@@ -101,8 +101,8 @@ def append_value_for_timestamp(existing_ts, new_ts):
 
     appended_ts =[]
 
-    logger.info("existing ts : ", existing_ts)
-    logger.info("new ts : ", new_ts)
+    logger.info("existing ts : ", len(existing_ts))
+    logger.info("new ts : ", len(new_ts))
     for i in range(len(existing_ts)):
         appended_ts.append(existing_ts[i])
         appended_ts[i].append(new_ts[i][1])
