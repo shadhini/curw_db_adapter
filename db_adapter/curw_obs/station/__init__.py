8a356132d1b6b1eed758abf251255366ea33e3e4
shadhini <jshadhiniaseka@gmail.com>
2019-07-24 09:59:26 +0530

    set append to True in curw_obs station utils

--- db_adapter/curw_obs/station/__init__.py
+++ db_adapter/curw_obs/station/__init__.py
@@ -1,3 +1,3 @@
 from .station_utils import get_station_id, get_station_by_id, add_station, delete_station_by_id, delete_station,\
-    add_stations, get_description, update_description, update_run_name
+    add_stations, get_description, update_description
 from .station_enum import StationEnum
