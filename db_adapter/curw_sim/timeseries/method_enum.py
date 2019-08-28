5936a43a1f2275a4fe77c13f702f073d1c0091c1
shadhini <jshadhiniaseka@gmail.com>
2019-08-28 11:56:24 +0530

    add new timeseries interpolation method TSF-Time Series Forecasting

--- db_adapter/curw_sim/timeseries/method_enum.py
+++ db_adapter/curw_sim/timeseries/method_enum.py
@@ -4,26 +4,30 @@ from enum import Enum
 class MethodEnum(Enum):
     MME = 'Multi_Model_Ensemble'
     SF = 'Statistical_Forecasting'
+    TSF = 'Time_Series_Forecasting'
     Other = 'Other'
 
     @staticmethod
     def getType(name):
         _nameToType = {
-                'Multi_Model_Ensemble': MethodEnum.MME,
-                'MME'                 : MethodEnum.MME,
-                'Statistical_Forecasting': MethodEnum.SF,
-                'SF'                  : MethodEnum.SF,
-                'Other'               : MethodEnum.Other
-                }
+            'Multi_Model_Ensemble': MethodEnum.MME,
+            'MME': MethodEnum.MME,
+            'Statistical_Forecasting': MethodEnum.SF,
+            'SF': MethodEnum.SF,
+            'Time_Series_Forecasting': MethodEnum.TSF,
+            'TSF': MethodEnum.TSF,
+            'Other': MethodEnum.Other
+        }
 
         return _nameToType.get(name, MethodEnum.Other)
 
     @staticmethod
     def getAbbreviation(name):
         _nameToAbbr = {
-                MethodEnum.MME  : 'MME',
-                MethodEnum.SF   : 'SF',
-                MethodEnum.Other: 'Other'
-                }
+            MethodEnum.MME: 'MME',
+            MethodEnum.SF: 'SF',
+            MethodEnum.TSF: 'TSF',
+            MethodEnum.Other: 'Other'
+        }
 
         return _nameToAbbr.get(name, 'Other')
