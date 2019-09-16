8825f17a272866daad5a47e120a53650cbab71e8
shadhini <jshadhiniaseka@gmail.com>
2019-09-16 17:19:44 +0530

    add utils to fill missing entries of a timeseries using specified interpolation method

--- db_adapter/curw_sim/common/ts_utils.py
+++ db_adapter/curw_sim/common/ts_utils.py
@@ -28,7 +28,15 @@ def fill_ts_missing_entries(start, end, timeseries, interpolation_method, timest
 
     # fill missing values using specified interpolation method
     processed_df = pd.to_numeric(df[df.columns[0]], errors='coerce')
-    return processed_df.interpolate(method=interpolation_method, limit_direction='both')
+    final_df = processed_df.interpolate(method=interpolation_method, limit_direction='both')
+    return final_df.reset_index().values.tolist()
+
+
+# TS2 = [["2019-08-22 01:00:00", 1.8], ["2019-08-22 02:30:00", 1.5], ["2019-08-22 03:30:00", 1.4],["2019-08-22 07:30:00", 2.4],
+#        ["2019-08-22 08:30:00", 2.5], ["2019-08-23 07:30:00", 2.5], ["2019-08-23 08:30:00", 2.5]]
+#
+# print(fill_ts_missing_entries(start="2019-08-22 00:00:00", end="2019-08-23 12:00:00", timeseries=TS2,
+#                               interpolation_method="linear", timestep=30))
 
 
 
