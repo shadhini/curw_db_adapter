ec9418928b5c85a54984997e859deae27aadf4a8
shadhini <jshadhiniaseka@gmail.com>
2019-09-28 18:48:21 +0530

    add function to get dataase connection which supports iterable cursor which would support in bulk data retrieval

--- db_adapter/base/pymysql_base.py
+++ db_adapter/base/pymysql_base.py
@@ -73,39 +73,7 @@ def execute_write_query(pool, query, params):
             connection.close()
 
 
-#  Conn = pool.connection()
-#  After each time you need a database connection, you can use the connection() function to get the connection.
-# cur=conn.cursor()
-# SQL="select * from table"
-# count=cur.execute(SQL)
-# results=cur.fetchall()
-# cur.close()
-# conn.close()
-
-
-# Connect to the database
-# connection = pymysql.connect(host='localhost',
-#                              user='user',
-#                              password='passwd',
-#                              db='db',
-#                              charset='utf8mb4',
-#                              cursorclass=pymysql.cursors.DictCursor)
-#
-# try:
-#     with connection.cursor() as cursor:
-#         # Create a new record
-#         sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
-#         cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
-#
-#     # connection is not autocommit by default. So you must commit to save
-#     # your changes.
-#     connection.commit()
-#
-#     with connection.cursor() as cursor:
-#         # Read a single record
-#         sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
-#         cursor.execute(sql, ('webmaster@python.org',))
-#         result = cursor.fetchone()
-#         print(result)
-# finally:
-#     connection.close()
+# for bulk data retrieval
+def get_connection_for_iterable_cursor(host, port, user, password, db):
+    return pymysql.connect(host=host, user=user, password=password, db=db, port=port,
+                           cursorclass=pymysql.cursors.DictCursor)
