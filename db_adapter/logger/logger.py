4c94b352cf25fc5bc4901c3033763c47f25698f7
shadhini <jshadhiniaseka@gmail.com>
2019-12-04 11:04:03 +0530

    use config file inside package for logging utils instead of an external file

--- db_adapter/logger/logger.py
+++ db_adapter/logger/logger.py
@@ -1,9 +1,13 @@
 import logging
 import logging.config
 import yaml
+import os
+
+this_dir, this_filename = os.path.split(__file__)
+logger_config_file_path = os.path.join(this_dir, "logger_config.yaml")
 
 
-with open('logger_config.yaml', 'r') as f:
+with open(logger_config_file_path, 'r') as f:
     config = yaml.safe_load(f.read())
     logging.config.dictConfig(config)
 
