e84f7fb17def5dc25dc0e4fc0ed71f2f74e389ce
shadhini <jshadhiniaseka@gmail.com>
2020-02-13 13:23:50 +0530

    add function to insert run metadata ionto curw_fcst database

--- db_adapter/curw_fcst/timeseries/timeseries.py
+++ db_adapter/curw_fcst/timeseries/timeseries.py
@@ -546,3 +546,35 @@ class Timeseries:
         finally:
             if connection is not None:
                 connection.close()
+
+    def insert_run_metadata(self, source_id, sim_tag, fgt, metadata):
+
+        """
+        Insert new run info entry
+        :param source_id:
+        :param sim_tag:
+        :param fgt:
+        :param metadata:
+        :return:
+        """
+
+        connection = self.pool.connection()
+        try:
+
+            with connection.cursor() as cursor:
+                sql_statement = "INSERT INTO `run_info` (`source`, `sim_tag`, `fgt`, `metadata`) " \
+                                "VALUES ( %s, %s, %s, %s)"
+                cursor.execute(sql_statement, ())
+
+            connection.commit()
+            return True
+        except Exception as exception:
+            connection.rollback()
+            error_message = "Insertion failed for run info enty with source={}, sim_tag={}, fgt={}, metadata={}"\
+                .format(source_id, sim_tag, fgt, json.dumps(metadata))
+            logger.error(error_message)
+            traceback.print_exc()
+            raise exception
+        finally:
+            if connection is not None:
+                connection.close()
